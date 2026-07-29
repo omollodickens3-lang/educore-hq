c6bbafea42c868c9150cda9148ac5dab311f8135
omollodickens3-lang <omollodickens3@gmail.com>
2026-07-29 08:01:48 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ import AttendancePage from './pages/AttendancePage';
 import AssignmentsPage from './pages/AssignmentsPage';
 import AssignmentDetailPage from './pages/AssignmentDetailPage';
 import AnalyticsPage from './pages/AnalyticsPage';
+import BroadsheetPage from './pages/BroadsheetPage';
 import ParentPortalPage from './pages/ParentPortalPage';
 import ParentPortalInfoPage from './pages/ParentPortalInfoPage';
 import ParentRegisterPage from './pages/ParentRegisterPage';
@@ -68,6 +69,7 @@ function AppRoutes() {
         <Route path="assignments"  element={<AssignmentsPage />} />
         <Route path="assignments/:id" element={<AssignmentDetailPage />} />
         <Route path="analytics" element={<AnalyticsPage />} />
+        <Route path="broadsheet" element={<BroadsheetPage />} />
         <Route path="content"      element={<ComingSoon title="✏️ Content Generation" />} />
         <Route path="super-admin"  element={<SuperAdminPage />} />
         <Route path="pending-registrations" element={<PendingRegistrationsPage />} />
